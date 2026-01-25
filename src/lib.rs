971f00f08c7fc1c3389383fa6b8443640e4b2099
synabler <synabler@proton.me>
2026-01-25 16:17:56 +0900

    load_movie: open .tar.gz

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,36 @@
 use std::{fs::File, path::Path};
 
+use flate2::read::GzDecoder;
+use tar::Archive;
+
 #[derive(Debug)]
 pub enum LoadError {
     FileError(std::io::Error),
+    InvalidArchive,
 }
 
 pub fn load_movie<P: AsRef<Path>>(path: P) -> Result<(), LoadError> {
-    match File::open(path) {
-        Ok(_file) => {
-            return Ok(());
-        }
+    // open the movie file as .tar.gz
+    let mut archive = match File::open(path) {
+        Ok(file) => Archive::new(GzDecoder::new(file)),
         Err(err) => {
             return Err(LoadError::FileError(err));
         }
+    };
+
+    let entries = match archive.entries() {
+        Ok(entries) => entries,
+        Err(err) => {
+            return Err(LoadError::FileError(err));
+        }
+    };
+
+    for entry in entries {
+        let Ok(entry) = entry else {
+            return Err(LoadError::InvalidArchive);
+        };
+        println!("{:?}", entry.path());
     }
+
+    return Ok(());
 }
--- tests/load.rs
+++ tests/load.rs
@@ -5,12 +5,22 @@ fn test_load() -> Result<(), LoadError> {
     load_movie("tests/movies/221769_Trapped_5.ltm")
 }
 
+/// If a file doesn't exist, it should fail with `NotFound`.
 #[test]
 fn test_load_not_exist() {
     match load_movie("tests/movies/nope.ltm") {
-        Ok(_) => panic!("should have failed to load"),
         Err(LoadError::FileError(err)) => {
             assert_eq!(err.kind(), std::io::ErrorKind::NotFound);
         }
+        _ => panic!("should have failed to load"),
+    }
+}
+
+/// If a file is not a .tar.gz file, it should fail with `InvalidArchive`.
+#[test]
+fn test_load_not_movie() {
+    match load_movie("tests/invalid_movies/not_movie.txt") {
+        Err(LoadError::InvalidArchive) => {}
+        _ => panic!("should have failed to load"),
     }
 }
